87cddb0df14f1a19ccfd29b258920a822b63e58f
Aleh Tanasiuk <atanasiuk@hubspot.com>
2021-07-14 13:06:36 +0300

    Add deps && linter

---
+++ node/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  extends: 'eslint:recommended',
+  root: true,
+  env: {
+    browser: false,
+    node: true,
+    commonjs: true,
+    es6: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {
+    'no-console': 'off',
+    'no-return-await': 'error',
+  }
+};
